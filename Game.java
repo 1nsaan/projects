49c401a9b4cb0d97759e091fa66405d4fa724435
Sachin James <110364595+msachinjames@users.noreply.github.com>
2023-04-05 14:52:14 +0530

    Update Game.java
    -moved components to a frame.
    -handled dragged click events to consider for evaluation

--- Game.java
+++ Game.java
@@ -1,51 +1,66 @@
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
-class Game extends MouseAdapter implements ActionListener{
-    JFrame frame;
+class CatchMe  implements ActionListener{
+	JFrame f;
     JButton button;
     JButton score;
     int clicks=0,successful_clicks=0;
-    int x,y,key,height=100;
+    int x,y,height=25;
     Random r;
 
-    Game(){
+    CatchMe(){
 
 	//creating components
-        frame =new JFrame("Catch me");
+		f=new JFrame("CatchMe");
         score= new JButton("Score: 0");
         button=new JButton("Start");
 
 
 	//configuring components
-	UIManager.getLookAndFeelDefaults().put("defaultFont",new Font("Arial",Font.BOLD,14));
+	//UIManager.getLookAndFeelDefaults().put("defaultFont",new Font("Arial",Font.BOLD,14));
 	button.setFocusPainted(false);
-	button.setBounds(800,800,300,150);
-	score.setBounds(800,400,300,150);
+	button.setBounds(600,400,300,150);
+	score.setBounds(600,400,300,150);
 	score.setBackground(Color.green);
 	score.setForeground(Color.white);
 	score.setFont(new Font("Arial",Font.BOLD,30));
 	r=new Random();
 
 	//this key decides with how many clicks should the size of the button change
-	key=r.nextInt(5)+1;
 
 	// adding components to frame
-        frame.add(button);
-        frame.add(score);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(button);
+        f.add(score);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         button.addActionListener(this);
-        frame.addMouseListener(this);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-	frame.setLayout(null);
-	frame.getContentPane().setBackground(Color.black);
-
+        f.addMouseListener(new MouseAdapter(){
+	public void mouseClicked(MouseEvent e){
+	   clicks+=1;
+	   checkGameStatus();
+	}}
+);
+    f.addMouseMotionListener(new MouseMotionAdapter(){
+		public void mouseDragged(MouseEvent me){
+			clicks+=1;
+			if(x>=me.getX()&& x<(me.getX()+100) && y==me.getY()){
+			  successful_clicks+=1;
+			  
+			}
+			System.out.println(x+" "+me.getX()+" "+y+" "+me.getY());
+			checkGameStatus();
+		}
+	});
+    f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	
+	f.getContentPane().setBackground(Color.white);
+	f.setLayout(null);
 	//setting visibility of components
 	score.setVisible(false);
-        frame.setVisible(true);
-
+	f.setSize(600,600);
+	f.setVisible(true);
+	
     }
     public void actionPerformed(ActionEvent e){
 
@@ -55,42 +70,33 @@ class Game extends MouseAdapter implements ActionListener{
          if(clicks==1) {
 		button.setText("Catch me!");
 		score.setVisible(false);
-		height=100;
 		}
 
 	//randomizing the position of button with each click
-        x=r.nextInt(800);
-        y=r.nextInt(800);
+        x=r.nextInt(600);
+        y=r.nextInt(600);
 
-	//the below conditional is for resizing the button
-	if(clicks%key==0 && clicks!=0)height=(int)(height>=15?height/3:100);
-
-
-        button.setBounds(x,y,height,height);
+	//the below conditional is for repositioning the button(temporary-thread based in next)
+        button.setBounds(x,y,90,height);
+		
         }
 
-
-    public void mouseClicked(MouseEvent e){
-
-	  clicks+=1;
-	  //the below conditional checks for the end-of-game condition
+    void checkGameStatus(){
+		  //the below conditional checks for the end-of-game condition
           if((clicks-successful_clicks)==1)
 	 {
 	    score.setVisible(true);
 	    score.setText("Score: "+(successful_clicks-1));
  	    successful_clicks=0;
 	    clicks=0;
-	    button.setBounds(800,800,300,150);
-	    score.setBounds(800,400,300,150);
-            button.setText("START");
+	    button.setBounds(600,600,300,150);
+        button.setText("START");
 	}
 	else
 	score.setText("Score: "+successful_clicks);
-	  
-	
-    }
+	}
     public static void main(String args[]){
-        new Game();
+        new CatchMe();
     }
 
 }
